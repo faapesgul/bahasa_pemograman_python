aca284aa84c04dd631e54c2ac21f9f101a68fc83
Fikri Alam <fikrialam3@student.esaunggul.ac.id>
2022-11-02 22:33:29 +0700

    Add: Tugas 6

---
+++ tugas/pert6/main.py
@@ -0,0 +1,15 @@
+def header():
+    print("=================")
+    print("PROGRAM QUIZ")
+    print("====================")
+
+
+header()
+
+
+def a():
+    nama = input("masukkan nama : ")
+    nim = input("masukkan nim : ")
+
+
+a()
